3ca48a723d974bbe34d04dd6432398839bd77108
sureshknxtwave <sureshknxtwave@gmail.com>
2024-01-16 17:54:50 +0000

    Counter App Using React JS

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,11 @@
+import {Component} from 'react'
+
+class MyComponent extends Component {
+  render() {
+    const {name} = this.props
+
+    return <h1>Hello, {name}</h1>
+  }
+}
+
+export default MyComponent
